86279d7ecdfb3def253e2bc86fbc0197a76ad1bb
jokerSharp <qasoftware@tutanota.com>
2024-05-18 22:46:44 +0300

    add second test

--- src/main/java/com/shop/PetProject/models/ProductEntity.java
+++ src/main/java/com/shop/PetProject/models/ProductEntity.java
@@ -1,10 +1,7 @@
 package com.shop.PetProject.models;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -15,6 +12,7 @@ import java.util.UUID;
 @Builder
 @Entity
 @Table(name = "product")
+@ToString
 public class ProductEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/test/java/com/shop/PetProject/services/ProductServiceTest.java
+++ src/test/java/com/shop/PetProject/services/ProductServiceTest.java
@@ -3,6 +3,7 @@ package com.shop.PetProject.services;
 import com.shop.PetProject.dtos.ProductDTO;
 import com.shop.PetProject.models.ProductEntity;
 import com.shop.PetProject.repositories.ProductRepository;
+import com.shop.PetProject.testUtils.builders.converters.ManualConverter;
 import com.shop.PetProject.utils.ProductNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -11,11 +12,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.ConversionService;
 
 import static com.shop.PetProject.testUtils.builders.ProductBuilder.getProductDTO;
-import static com.shop.PetProject.testUtils.builders.ProductBuilder.getProductEntity;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
@@ -29,10 +31,13 @@ public class ProductServiceTest {
 
     @Test
     void saveProduct_validProduct_saveProduct() {
-        ProductEntity productEntity = getProductEntity();
         ProductDTO productDTO = getProductDTO();
+        ProductEntity productEntity = ManualConverter.convert(productDTO);
+        when(conversionService.convert(productDTO, ProductEntity.class))
+                .thenReturn(productEntity);
         productService.saveProduct(productDTO);
-        Mockito.verify(productRepository, Mockito.times(1)).save(conversionService.convert(productDTO, ProductEntity.class));
+        verify(productRepository, Mockito.times(1))
+                .save(any(ProductEntity.class));
     }
 
     @Test
