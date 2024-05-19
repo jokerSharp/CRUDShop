4d7182e14d94e7e8491da823ce08f2ebcccfa025
jokerSharp <qasoftware@tutanota.com>
2024-05-19 13:11:04 +0300

    add third test

--- src/test/java/com/shop/PetProject/services/ProductServiceTest.java
+++ src/test/java/com/shop/PetProject/services/ProductServiceTest.java
@@ -14,10 +14,11 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.core.convert.ConversionService;
 
+import java.util.List;
+
 import static com.shop.PetProject.testUtils.builders.ProductBuilder.getProductDTO;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
@@ -36,8 +37,17 @@ public class ProductServiceTest {
         when(conversionService.convert(productDTO, ProductEntity.class))
                 .thenReturn(productEntity);
         productService.saveProduct(productDTO);
-        verify(productRepository, Mockito.times(1))
+        verify(productRepository, times(1))
                 .save(any(ProductEntity.class));
+    }
+
+    @Test
+    void getProductByName_productExists_getProduct() {
+        ProductDTO productDTO = getProductDTO();
+        ProductEntity productEntity = ManualConverter.convert(productDTO);
+        Mockito.when(productRepository.findByName(productEntity.getName())).thenReturn(List.of(productEntity));
+        productService.getProductByName(productEntity.getName());
+        verify(productRepository, times(1)).findByName(anyString());
     }
 
     @Test
