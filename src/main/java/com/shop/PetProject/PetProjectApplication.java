bde1ccb88318c85e0b914bd42e15d4e5f855d042
jokerSharp <qasoftware@tutanota.com>
2024-05-28 21:26:29 +0300

    add caching of the currency exchange rate with caffein library

--- src/main/java/com/shop/PetProject/PetProjectApplication.java
+++ src/main/java/com/shop/PetProject/PetProjectApplication.java
@@ -2,7 +2,10 @@ package com.shop.PetProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableCaching
 @SpringBootApplication
 public class PetProjectApplication {
 
--- src/main/java/com/shop/PetProject/exchange/ExchangeRate.java
+++ src/main/java/com/shop/PetProject/exchange/ExchangeRate.java
@@ -3,10 +3,12 @@ package com.shop.PetProject.exchange;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Data
 public class ExchangeRate {
     private BigDecimal usdExchangeRate;
     private BigDecimal eurExchangeRate;
     private BigDecimal gbpExchangeRate;
+    private LocalDateTime creationTime;
 }
