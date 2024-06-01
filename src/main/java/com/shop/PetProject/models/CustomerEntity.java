33d2cc2aadafde83163b0af6f1c96600169abc70
jokerSharp <qasoftware@tutanota.com>
2024-06-01 22:28:47 +0300

    add customer entity

---
+++ src/main/java/com/shop/PetProject/models/CustomerEntity.java
@@ -0,0 +1,24 @@
+package com.shop.PetProject.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "customer")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "email")
+    private String email;
+}
