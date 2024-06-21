58c76a383d581bfb590197b27b6443b49fde6842
jokerSharp <qasoftware@tutanota.com>
2024-06-21 20:52:37 +0300

    add customer validation

--- src/main/java/com/shop/PetProject/controllers/CustomerController.java
+++ src/main/java/com/shop/PetProject/controllers/CustomerController.java
@@ -1,20 +1,30 @@
 package com.shop.PetProject.controllers;
 
+import com.shop.PetProject.dtos.PageResponse;
 import com.shop.PetProject.dtos.customer.CustomerDTO;
 import com.shop.PetProject.dtos.customer.CustomerFilter;
-import com.shop.PetProject.dtos.PageResponse;
+import com.shop.PetProject.exceptions.customer.CustomerEmailAlreadyExistsException;
 import com.shop.PetProject.services.CustomerService;
+import com.shop.PetProject.utils.CustomerValidator;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/customer")
 public class CustomerController {
     private final CustomerService customerService;
+    private final CustomerValidator customerValidator;
 
-    public CustomerController(CustomerService customerService) {
+    public CustomerController(CustomerService customerService, CustomerValidator customerValidator) {
         this.customerService = customerService;
+        this.customerValidator = customerValidator;
     }
 
     @GetMapping
@@ -30,7 +40,15 @@ public class CustomerController {
     }
 
     @PostMapping
-    public void save(@RequestBody CustomerDTO customerDTO) {
+    public void save(@RequestBody @Valid CustomerDTO customerDTO, BindingResult bindingResult) {
+        customerValidator.validate(customerDTO, bindingResult);
+        if (bindingResult.hasErrors()) {
+            List<FieldError> errors = bindingResult.getFieldErrors();
+            String errorMsg = errors.stream()
+                    .map(error -> error.getField() + " - " + error.getDefaultMessage() + ";")
+                    .collect(Collectors.joining());
+            throw new CustomerEmailAlreadyExistsException(errorMsg);
+        }
         customerService.save(customerDTO);
     }
 
